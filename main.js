216416a68a9266d004ae3fa907854b3268213521
maira naseer ud din <161260376+mairanaseer@users.noreply.github.com>
2024-09-02 22:51:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+////new example 
+let isvictory = false;
+const cricket = () => {
+    return new Promise((resolve, reject) => {
+        if (isvictory) {
+            setTimeout(() => {
+                resolve("we won");
+            }, 1500);
+        }
+        else {
+            console.log("match in progress");
+            setTimeout(() => {
+                reject("tm se na ho payga");
+            }, 1500);
+        }
+        ;
+    });
+};
+cricket()
+    .then((responce) => {
+    console.log(responce);
+})
+    .catch((err) => {
+    console.log(err);
+});
+/////chainin 
